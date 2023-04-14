5ab217c171c0360b0b3fac38831ef502974f8971
qwqcxh <qwqcxh@gmail.com>
2023-04-14 17:18:56 +0800

    add search bar

---
+++ assets/js/searchbar.js
@@ -0,0 +1,43 @@
+
+$(function () {
+  var posts = [];
+  $.get('assets/js/posts.json', function (data) {
+    posts = data;
+  });
+  $('#search').on('keyup', function () {
+    var keyword = this.value.toLowerCase();
+    var searchResult = [];
+
+    if (keyword.length > 0) {
+      $('#search-result').show();
+    } else {
+      $('#search-result').hide();
+    }
+    $('.result-item').remove();
+
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+      if (
+        post.title.toLowerCase().indexOf(keyword) >= 0 ||
+        post.description.toLowerCase().indexOf(keyword) >= 0
+      ) {
+        searchResult.push(post);
+      }
+    }
+    if (searchResult.length === 0) {
+      $('#search-result').append(
+        '<div class="result-item">There is no search result.</div>'
+      );
+    } else {
+      for (var i = 0; i < searchResult.length; i++) {
+        $('#search-result').append(
+          '<a class="result-item" href="' +
+            searchResult[i].url +
+            '"><div class="title">【' + searchResult[i].category + '】' +
+            searchResult[i].title +
+            '</div></a>'
+        );
+      }
+    }
+  });
+});
